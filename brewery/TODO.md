ebbdc6bc4f0c00dff9ba060a788d48de64595e6d
mkuchenbecker <mkuchenbecker@lyft.com>
2019-01-17 11:16:10 -0800

    Initial commit

---
+++ brewery/TODO.md
@@ -0,0 +1,40 @@
+Coding:
+* enforce linter (golangci)
+* enforce 90% test coverage on master
+* enable linters on pre-merge
+
+
+Persistance layer:
+
+* Persist config to (remote?) database and retrieve.
+* Persist all temperature reads to database
+
+Visualization:
+* Dashboard to visualize temperatures (grafana)?
+
+Deploys:
+
+* Deploy to RPI via Kubernetes
+* Deploy serverless code to Google Cloud
+
+Experiment/ Fun:
+
+* Use redis cluster to cache current brewery setup
+
+
+Stabilize
+
+* Make all GRPC calls retry.
+* Make the number of retries a constant.
+* Make an implementation to the element interface that locks, unlocks, and turns itself off.
+
+
+Hardware:
+
+* Get DS18B20 running.
+* Get holes cut in box for ports.
+
+Feature work:
+
+* Pumps
+* Pump config
---
+++ brewery/rpi/element/heater.go
@@ -0,0 +1,28 @@
+package element
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	model "github.com/mkuchenbecker/brewery3/brewery/model/gomodel"
+	gpio "github.com/mkuchenbecker/brewery3/brewery/rpi/gpio/igpio"
+)
+
+// HeaterServer implements switch.
+type HeaterServer struct {
+	ctrl gpio.Controller
+	pin  int
+}
+
+func (s *HeaterServer) On(ctx context.Context, req *model.OnRequest) (*model.OnResponse, error) {
+	fmt.Printf("On: %s - %s\n", "req.ID", time.Now().String())
+	err := s.ctrl.PowerPin(s.pin, true)
+	return &model.OnResponse{}, err
+}
+
+func (s *HeaterServer) Off(ctx context.Context, req *model.OffRequest) (*model.OffResponse, error) {
+	fmt.Printf("Off: %s - %s\n", "req.ID", time.Now().String())
+	err := s.ctrl.PowerPin(s.pin, false)
+	return &model.OffResponse{}, err
+}
