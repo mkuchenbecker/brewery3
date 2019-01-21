20e8c2f20e9aefc1aa29d618ad0390742d6f2bc0
mkuchenbecker <mkuchenbecker@lyft.com>
2019-01-21 07:47:55 -0800

    Git Smash

--- brewctl.go
+++ brewctl.go
@@ -19,7 +19,7 @@ func MakeTemperatureClient(port int, address string) model.ThermometerClient {
 	fmt.Printf("Waiting for discovery on port: %d\n", port)
 	time.Sleep(5 * time.Second)
 	fmt.Printf("Connecting to client: %d\n", port)
-	conn, err := grpc.Dial(fmt.Sprintf("localhost://%d", port), grpc.WithInsecure())
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
@@ -39,7 +39,7 @@ func MakeSwitchClient(port int, pin int) model.SwitchClient {
 	fmt.Printf("Waiting for discovery on port: %d\n", port)
 	time.Sleep(5 * time.Second)
 	fmt.Printf("Connecting to client: %d\n", port)
-	conn, err := grpc.Dial(fmt.Sprintf("localhost://%d", port), grpc.WithInsecure())
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
