0025e33a8317ca64462cfa2aa8a83591daefcf20
Mike Kuchenbecker <mkuchenbecker@lyft.com>
2019-08-14 13:59:13 -0700

    .

--- brewery/logger/influx_test.go
+++ brewery/logger/influx_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/mkuchenbecker/brewery3/brewery/utils"
 )
 
@@ -11,7 +13,7 @@ func TestFake(t *testing.T) {
 	// This test is simply exercising the fake code to ensure a fake can be called
 	// with no setup required, and that the current default is a fake.
 	fake := NewFake()
-	fake.InsertTemperature(context.Background(), utils.NewTemperaturePointSink())
+	assert.NoError(t, fake.InsertTemperature(context.Background(), utils.NewTemperaturePointSink()))
 	fake2 := NewDefault()
-	fake2.InsertTemperature(context.Background(), utils.NewTemperaturePointSink())
+	assert.NoError(t, fake2.InsertTemperature(context.Background(), utils.NewTemperaturePointSink()))
 }
