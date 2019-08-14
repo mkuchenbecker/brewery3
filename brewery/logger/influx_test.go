66224e376e0f3381d67b8c3cbee07ac6e565acdb
Mike Kuchenbecker <mkuchenbecker@lyft.com>
2019-08-14 13:58:11 -0700

    .

--- brewery/logger/influx_test.go
+++ brewery/logger/influx_test.go
@@ -1,1 +1,17 @@
 package logger
+
+import (
+	"context"
+	"testing"
+
+	"github.com/mkuchenbecker/brewery3/brewery/utils"
+)
+
+func TestFake(t *testing.T) {
+	// This test is simply exercising the fake code to ensure a fake can be called
+	// with no setup required, and that the current default is a fake.
+	fake := NewFake()
+	fake.InsertTemperature(context.Background(), utils.NewTemperaturePointSink())
+	fake2 := NewDefault()
+	fake2.InsertTemperature(context.Background(), utils.NewTemperaturePointSink())
+}
