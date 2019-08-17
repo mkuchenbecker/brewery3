09c529d197d1722a2ad3e373786546b5e5febd64
Mike Kuchenbecker <mkuch90@gmail.com>
2019-08-17 11:57:56 -0700

    Git Smash

--- start_kube.sh
+++ start_kube.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+rm -rf $HOME/.kube
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
