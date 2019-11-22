fa74ac5921075b9b22e036745080b12034397b67
Mike Kuchenbecker <mkuch90@gmail.com>
2019-11-21 21:27:16 -0800

    .

--- brewery/cli/main.go
+++ brewery/cli/main.go
@@ -2,28 +2,17 @@
 
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/mkuchenbecker/brewery3/brewery/cli/cli"
-	"github.com/mkuchenbecker/brewery3/brewery/utils"
-
-	model "github.com/mkuchenbecker/brewery3/brewery/model/gomodel"
-	"google.golang.org/grpc"
-)
-
 func main() {
-	conn, err := grpc.Dial(fmt.Sprintf("brewpi:9000"),
-		grpc.WithInsecure())
-	if err != nil {
-		panic(err)
-	}
-	defer utils.DeferErrReturn(conn.Close, &err)
-	client := model.NewBreweryClient(conn)
-	err = cli.Run(client, os.Args)
-	if err != nil {
-		utils.LogError(nil, err, "encountered an error")
-		os.Exit(1)
-	}
+	// conn, err := grpc.Dial(fmt.Sprintf("brewpi:9000"),
+	// 	grpc.WithInsecure())
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer utils.DeferErrReturn(conn.Close, &err)
+	// client := model.NewBreweryClient(conn)
+	// err = cli.Run(client, os.Args)
+	// if err != nil {
+	// 	utils.LogError(nil, err, "encountered an error")
+	// 	os.Exit(1)
+	// }
 }
