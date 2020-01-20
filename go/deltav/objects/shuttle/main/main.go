a7183a7e217183d9a98d0813f154849a7e68ef38
Mike Kuchenbecker <mkuch90@gmail.com>
2020-01-20 07:01:05 -0800

    Adding DeltaV

---
+++ go/deltav/objects/shuttle/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	protos "github.com/golang001/deltav/model/gomodel"
+	"github.com/golang001/deltav/objects/shuttle"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := protos.NewWorldModelClient(conn)
+
+	vessel := protos.Vessel{
+		Sensors: &protos.SensorSystem{
+			Sensors: []*protos.Sensor{
+				{
+					SensorType:    protos.Sensor_PASSIVE,
+					RadiationType: protos.RadiationType_GAMMA,
+				},
+			},
+		},
+	}
+	_, err = shuttle.NewPlayerShuttle(vessel, c)
+	if err != nil {
+		log.Fatalf("could not create user: %v", err)
+
+	}
+	r, err := c.Register(context.Background(), &protos.RegisterRequest{})
+	if err != nil {
+		log.Fatalf("could not register: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Effect)
+}
---
+++ python/vector.py
@@ -0,0 +1,11 @@
+class Vector:
+    x = 0
+    y = 0
+    z = 0
+    t = 0
+
+    def __init__(self, x, y, z, t):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
