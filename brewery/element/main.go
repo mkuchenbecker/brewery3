d7647eda02c56d8f4f1752ab96ca287d850858ca
Mike Kuchenbecker <mkuch90@gmail.com>
2020-02-15 11:44:45 -0800

    fix

--- brewery/element/main.go
+++ brewery/element/main.go
@@ -6,52 +6,28 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
-	"strconv"
 
-	"github.com/mkuchenbecker/brewery3/brewery/gpio"
+	"github.com/mkuchenbecker/brewery3/brewery/element/element"
 	"github.com/mkuchenbecker/brewery3/brewery/gpio/integration"
 	model "github.com/mkuchenbecker/brewery3/brewery/model/gomodel"
-	"github.com/mkuchenbecker/brewery3/brewery/servers/sensors"
-	"github.com/mkuchenbecker/brewery3/brewery/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-func startThermometer(port int, address string) { // nolint: deadcode
+func main() { // nolint: deadcode
+	port := 9100
+	pin := uint8(1)
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v\n", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 	serve := grpc.NewServer()
-	addr := gpio.TemperatureAddress(address)
-	err = integration.VerifyTemperatureAddress(addr)
-	if err != nil {
-		utils.LogError(&utils.DefualtPrinter{}, err, "failed to read address")
-	}
-	server, err := sensors.NewThermometerServer(integration.NewDefaultController(), addr)
-	if err != nil {
-		utils.LogError(&utils.DefualtPrinter{}, err, "failed to temperature")
-	}
-
-	model.RegisterThermometerServer(serve, server)
-
-	utils.Print("Serving Traffic")
-
+	heater := element.NewHeaterServer(integration.NewDefaultController(), pin)
+	model.RegisterSwitchServer(serve, heater)
+	// Register reflection service on gRPC server.
 	reflection.Register(serve)
 	if err := serve.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v\n", err)
+		log.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func main() {
-	strPort := os.Getenv("THERM_PORT")
-	address := os.Getenv("THERM_ADDR")
-
-	port, err := strconv.ParseInt(strPort, 10, 32)
-	if err != nil {
-		log.Fatalf("Invalid port is not 32 bit int: %s", strPort)
-	}
-
-	startThermometer(int(port), address)
 }
