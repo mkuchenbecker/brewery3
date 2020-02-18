6a8c313d09710f847af74c0e8fa9544fb00c770d
Mike Kuchenbecker <mkuch90@gmail.com>
2020-02-17 16:41:10 -0800

    Git Smash

--- brewery/start_thermometers.sh
+++ brewery/start_thermometers.sh
@@ -1,1 +1,15 @@
-MASH_THERM_ADDRESS_0=0
+docker run -d mkuchenbecker/brewery3:thermometer-latest --env THERM_PORT="9110" --env THERM_ADDR="28-0315712c08ff"
+
+	strPort := os.Getenv("THERM_PORT")
+	address := os.Getenv("THERM_ADDR")
+
+            name: THERM_PORT="9110"
+          - name: THERM_ADDR= "28-0315712c08ff"
+
+
+          - name: THERM_PORT="9111"
+          - name: THERM_ADDR= "28-0315715039ff"
+
+
+          - name: THERM_PORT="9112"
+          - name: THERM_ADDR= "28-031571188aff"
--- scripts/start_kube.sh
+++ scripts/start_kube.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-rm -rf $HOME/.kube
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 &&
+  rm -rf $HOME/.kube &&
+  mkdir -p $HOME/.kube &&
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config &&
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
