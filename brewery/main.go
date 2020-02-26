dab00a1f45dbad9e4be83e0801a5b11011768440
Mike Kuchenbecker <mkuch90@gmail.com>
2020-02-26 10:46:18 -0800

    Git Smash

--- brewery/main.go
+++ brewery/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"github.com/mkuchenbecker/brewery3/brewery/element/element"
+	"log"
+	"time"
+
+	"github.com/mkuchenbecker/brewery3/brewery/gpio/integration"
 )
 
 func main() {
-	element.StartElement(5, 9100)
+	pins := integration.DefaultPins{}
+	if err := pins.Open(); err != nil {
+		log.Fatalf("failed to open gpio: %v", err)
+	}
+	defer pins.Close()
+	pin := pins.Pin(uint8(5))
+	pin.Output()
+	for {
+		pin.High()
+		time.Sleep(time.Second)
+		pin.Low()
+		time.Sleep(time.Second)
+	}
 }
